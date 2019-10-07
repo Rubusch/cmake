f2ce0a568d87d7a429f4897aa20538db754bf618
Lothar Rubusch <l.rubusch@gmx.ch>
2019-10-07 16:40:04 +0200

    libraries: extern library example started

---
+++ 30__extern-libraries/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "config.h" /* generated, version number */
+
+using namespace std;
+
+int main()
+{
+  cout << "version number, major: " << Basic_VERSION_MAJOR << ", minor: " << (int)Basic_VERSION_MINOR << endl;
+
+  return 0;
+}
