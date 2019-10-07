92c03bc7736e72122d4ef33fb18e6eb5f8ad7881
Lothar Rubusch <l.rubusch@gmx.ch>
2019-10-07 17:10:57 +0200

    library-optional: update

--- 35__extern-libraries-optional/main.cpp
+++ 35__extern-libraries-optional/main.cpp
@@ -5,7 +5,10 @@
 
 #include "config.h" /* generated, version number */
 
+#ifdef USE_MYLIB
 #include "a.h"
+#endif
+
 
 
 using namespace std;
@@ -14,11 +17,15 @@ int main()
 {
   cout << "version number, major: " << Basic_VERSION_MAJOR << ", minor: " << Basic_VERSION_MINOR << endl;
 
+#ifdef USE_MYMATH
   auto a = new A();
+#endif
 
   cout << "a created..." << endl;
 
+#include "MathFunctions.h"
   delete a;
+#endif
 
   return 0;
 }
