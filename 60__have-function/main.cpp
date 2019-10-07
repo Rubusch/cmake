87a0fc0f097e2164913254fb26af82517f0c4838
Lothar Rubusch <l.rubusch@gmx.ch>
2019-10-07 19:35:25 +0200

    function-check: started

---
+++ 60__have-function/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "do ut des!" << endl;
+
+#if defined (HAVE_LOG)
+  cout << "log() is defined" << endl;
+#else
+  cout << "log() is not defined" << endl;
+#endif
+
+#if defined (HAVE_EXP)
+  cout << "exp() is defined" << endl;
+#else
+  cout << "exp() is not defined" << endl;
+#endl
+
+  return 0;
+}
