071815a831394b0f35723b57e9ac9a534e50d91a
Lothar Rubusch <l.rubusch@gmx.ch>
2019-10-09 19:29:56 +0200

    renaming

---
+++ 037__third-party-library/include/ChuckNorris.h
@@ -0,0 +1,27 @@
+/*
+  using extern library 'sqlite3'
+
+  RESOURCE
+  https://dmerej.info/blog/post/chuck-norris-part-1-cmake-ninja
+  original example by Dimitri Merejkowsky
+ */
+#pragma once
+
+#include <string>
+#include <sqlite3.h>
+
+class ChuckNorris
+{
+public:
+  ChuckNorris() = default;
+  ChuckNorris(ChuckNorris const&) = delete;
+  ChuckNorris(ChuckNorris &&) = delete;
+  ChuckNorris& operator=(ChuckNorris const&) = delete;
+  ChuckNorris& operator=(ChuckNorris &&) = delete;
+  ~ChuckNorris();
+
+  std::string getFact();
+
+private:
+  sqlite3* _db;
+};
