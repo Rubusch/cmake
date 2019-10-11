1a8673c01c60b46515009112fcb03cf9b181f086
Lothar Rubusch <l.rubusch@gmx.ch>
2019-10-11 15:33:51 +0200

    modular: top-down-build running

--- 090__modularized/build.sh
+++ 090__modularized/build.sh
@@ -1,3 +1,50 @@
 #!/bin/bash -e
 
-echo "READY."
+die()
+{
+    echo "$@"
+    exit 0
+}
+
+WHERE=$(dirname $0)
+WHERE=$(readlink -f $WHERE)
+echo $WHERE
+
+test -z "${WHERE}" && die "script location was / ??!!!"
+test -d "${WHERE}/build" && rm -rf "${WHERE}/build"
+
+
+## build top-down
+mkdir "${WHERE}/build"
+pushd $WHERE/build &> /dev/null
+export PREFIX="$(pwd)"
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+ninja
+ninja install
+
+
+## build piece by piece...
+# mkdir "${WHERE}/build"
+# pushd $WHERE/build &> /dev/null
+# export PREFIX="$(pwd)"
+# cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ../engine_lib
+# ninja
+# ninja install
+# rm CMakeCache.txt
+# cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ../display_lib
+# ninja
+# ninja install
+# rm CMakeCache.txt
+# cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ../navigation_lib
+# ninja
+# ninja install
+# rm CMakeCache.txt
+# cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+# ninja
+# ninja install
+
+./bin/spaceship.exe
+
+popd &> /dev/null
+echo
+echo "build.sh - READY."
