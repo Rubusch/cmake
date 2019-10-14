dad86888df53144dd964df9a3114efb232f95aea
Lothar Rubusch <l.rubusch@gmx.ch>
2019-10-14 16:46:46 +0200

    dynamic: example doing a shared library based build

--- 095__modularized-shared-lib__TODO/build.sh
+++ 095__modularized-shared-lib__TODO/build.sh
@@ -32,7 +32,7 @@ test -d "${WHERE}/build" && rm -rf "${WHERE}/build"
 mkdir "${WHERE}/build"
 pushd $WHERE/build &> /dev/null
 export PREFIX="$(pwd)"
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 ninja
 ninja install
 ./bin/spaceship.exe
@@ -45,19 +45,19 @@ test -d "${WHERE}/build" && rm -rf "${WHERE}/build"
 mkdir "${WHERE}/build"
 pushd $WHERE/build &> /dev/null
 export PREFIX="$(pwd)"
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ../capacitor_lib
+cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../capacitor_lib
 ninja
 ninja install
 rm CMakeCache.txt
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ../warp_lib
+cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../warp_lib
 ninja
 ninja install
 rm CMakeCache.txt
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ../navigation_lib
+cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../navigation_lib
 ninja
 ninja install
 rm CMakeCache.txt
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 ninja
 ninja install
 ./bin/spaceship.exe
@@ -65,24 +65,24 @@ popd &> /dev/null
 
 sleep 1
 
-## build piece by piece...
+## build bottom up
 test -d "${WHERE}/build" && rm -rf "${WHERE}/build"
 mkdir "${WHERE}/build"
 pushd $WHERE/build &> /dev/null
 export PREFIX="$(pwd)"
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../capacitor_lib
+cmake -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../capacitor_lib
 make
 make install
 rm CMakeCache.txt
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../warp_lib
+cmake -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../warp_lib
 make
 make install
 rm CMakeCache.txt
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../navigation_lib
+cmake -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ../navigation_lib
 make
 make install
 rm CMakeCache.txt
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DCMAKE_TOOLCHAIN_FILE=${WHERE}/cmake/toolchain.cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
 make install
 ./bin/spaceship.exe
